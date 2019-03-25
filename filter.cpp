03ec6cf9423298f0471a94049e9815a26d662f0d
nabaskes <nabaskes@uchicago.edu>
2019-03-25 13:35:16 -0400

    moved test code elsewhere

--- filter.cpp
+++ filter.cpp
@@ -8,7 +8,6 @@
 
 #include <vector>
 #include <stdio.h>
-using namespace std;
 
 
 std::vector< double > filtic(std::vector<double> b,
@@ -120,31 +119,4 @@ std::vector <double> filter(std::vector<double> b,
   }
 
   return y;
-}
-
-int main() {
-  vector<double> x, b, a;
-  x.reserve(100);
-  for(int i=0; i<100; i++) {
-    x.push_back(i);
-  }
-
-  b.reserve(1);
-  a.reserve(1);
-  b.push_back(1);
-  b.push_back(2);
-  a.push_back(1);
-  a.push_back(5);
-
-  vector <double> y = filter(b, a, x);
-
-
-
-  vector<double>::iterator it;
-  for(it=y.begin(); it != y.end(); it++) {
-    printf("%f\n", *it);
-  }
-
-
-  return 0;
 }
---
+++ tests.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <stdio.h>
+#include "filter.cpp"
+
+int main() {
+  std::vector<double> x, b, a;
+  x.reserve(100);
+  for(int i=0; i<100; i++) {
+    x.push_back(i);
+  }
+
+  b.reserve(1);
+  a.reserve(1);
+  b.push_back(1);
+  b.push_back(2);
+  a.push_back(1);
+  a.push_back(5);
+
+  std::vector <double> y = filter(b, a, x);
+
+
+
+  std::vector<double>::iterator it;
+  for(it=y.begin(); it != y.end(); it++) {
+    printf("%f\n", *it);
+  }
+
+
+  return 0;
+}
